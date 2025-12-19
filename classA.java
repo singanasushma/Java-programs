a192d4ba363e309c4686bf253633d90803338ace
singana sushma <Singanasushma2002@gmail.com>
2025-12-19 14:46:53 +0530

    Add files via upload

---
+++ classA.java
@@ -0,0 +1,13 @@
+
+public class classA{
+    void show(){
+        System.out.println("default mathod in pack1");
+    }
+
+}
+ class mainclass11{
+    public static void main(String[]args){
+        classA obj=new classA();
+        obj.show();
+}
+}
