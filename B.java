db15d11712fe83c91abaceb4a3827c1ffa54e804
singana sushma <Singanasushma2002@gmail.com>
2026-01-05 14:58:46 +0530

    Add files via upload

---
+++ B.java
@@ -0,0 +1,15 @@
+package pack2;
+
+import pack1.A11;
+
+public class B extends A11{
+
+    public static void main(String[] args) {
+
+        A11 obj = new A11();
+        System.out.println(obj.a);  
+
+        B b1 = new B();
+        System.out.println(b1.b);   
+    }
+}
