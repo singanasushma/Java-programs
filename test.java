4976882b33c82a32c3e07f663bb083ac3ccb6922
singana sushma <Singanasushma2002@gmail.com>
2026-01-05 14:57:36 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test{
+    public void show(){
+        System.out.println("Class test");
+    }
+}
+public class A{
+    public void display(){
+        System.out.println("Class A");
+    }
+}
