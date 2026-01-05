50110022058d3d17869b4ee1a406011032e7d3ee
singana sushma <Singanasushma2002@gmail.com>
2026-01-05 14:58:14 +0530

    Add files via upload

---
+++ A11.java
@@ -0,0 +1,13 @@
+package pack1;
+public class A11 {
+public int a = 10;
+protected int b = 20;
+          int c = 30;     
+private int d = 40;
+public void showAll() {
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+}
+}
